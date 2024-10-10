06a76d18a790abf0eea18ef64f0af12c36c7410a
Betânia Lobo Viana <betanialoboviana@gmail.com>
2024-10-09 21:32:20 -0300

    Add files via upload

---
+++ Fatorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fatorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fatorial(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    printf("Digite um número para calcular o fatorial: ");
+    scanf("%d", &n);
+    printf("O fatorial de %d é: %d\n", n, fatorial(n));
+    return 0;
+}
